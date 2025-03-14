717dd6b73b39a2eb154130fae9629771c4c79b95
iizukar <czechia.deitan@gmail.com>
2025-03-14 16:47:18 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,13 +8,16 @@ sleep 2
 DISPLAY=:99 fluxbox &
 sleep 2
 
+# Automatically launch Chromium in the background
+DISPLAY=:99 chromium --no-sandbox --disable-gpu &
+
 # Start websockify in the background
 # Serves noVNC (HTML/JS) from /noVNC on port 8080, bridging VNC at 5900
 websockify -D --web=/noVNC 0.0.0.0:8080 localhost:5900
 sleep 1
 
 # Finally, start the VNC server on display :99
-# -nopw   -> no password
+# -nopw    -> no password
 # -forever -> never exit
-# -shared -> allow multiple clients
+# -shared  -> allow multiple clients
 x11vnc -display :99 -nopw -forever -shared
