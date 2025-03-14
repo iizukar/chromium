032a66ab062f9a8a9f09f0d15d75a3235dbdafe5
iizukar <czechia.deitan@gmail.com>
2025-03-14 15:47:01 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-# Set VNC settings
+# VNC configuration
 mkdir -p ~/.vnc
-echo -e "password\npassword\nn" | vncpasswd
+echo -e "mypassword\nmypassword\nn" | vncpasswd
 chmod 600 ~/.vnc/passwd
 
-# Start Xvfb and VNC server
-Xvfb :1 -screen 0 1280x720x16 &
+# Start X virtual framebuffer
+Xvfb :1 -screen 0 1280x720x16 +extension RANDR +extension GLX &
 export DISPLAY=:1
 
-# Start window manager and VNC
+# Start window manager
 fluxbox &
+
+# Start VNC server
 vncserver :1 -geometry 1280x720 -depth 16 -localhost no \
   -xstartup /usr/bin/startfluxbox &
 
 # Start noVNC proxy
 /opt/noVNC/utils/novnc_proxy --vnc localhost:5901 --listen 0.0.0.0:8080 &
 
-# Start Chromium with proper flags
-chromium-browser --no-sandbox --disable-gpu \
+# Start Chromium with security flags
+chromium-browser \
+  --no-sandbox \
+  --disable-gpu \
+  --disable-features=AdjustOOMScore \
+  --disable-oom-kill-denial \
+  --no-zygote \
+  --disable-software-rasterizer \
   --display=$DISPLAY \
   --no-first-run \
   --start-maximized \
-  --window-size=1280,720
+  --window-size=1280,720 \
+  --remote-debugging-port=9222
