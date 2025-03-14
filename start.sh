7bc8b9b6d65a0017bfbec994bfb0047978ae9fd9
iizukar <czechia.deitan@gmail.com>
2025-03-14 15:53:52 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+# Create X authority file
+touch ~/.Xauthority
+xauth generate :1 . trusted
+
+# Generate Fluxbox config
+mkdir -p ~/.fluxbox
+echo "[session] (fluxbox)" > ~/.fluxbox/init
+echo "[begin] (fluxbox)" >> ~/.fluxbox/init
+echo "[end]" >> ~/.fluxbox/init
+
 # VNC configuration
 mkdir -p ~/.vnc
 echo -e "mypassword\nmypassword\nn" | vncpasswd
 chmod 600 ~/.vnc/passwd
 
+# Start DBus
+sudo mkdir -p /var/run/dbus
+sudo dbus-daemon --system --fork
+
 # Start X virtual framebuffer
 Xvfb :1 -screen 0 1280x720x16 +extension RANDR +extension GLX &
 export DISPLAY=:1
 
-# Start window manager
+# Start Fluxbox
 fluxbox &
 
 # Start VNC server
@@ -17,16 +31,17 @@ vncserver :1 -geometry 1280x720 -depth 16 -localhost no \
   -xstartup /usr/bin/startfluxbox &
 
 # Start noVNC proxy
-/opt/noVNC/utils/novnc_proxy --vnc localhost:5901 --listen 0.0.0.0:8080 &
+/home/chromiumuser/noVNC/utils/novnc_proxy --vnc localhost:5901 --listen 0.0.0.0:8080 &
 
 # Start Chromium with security flags
 chromium-browser \
   --no-sandbox \
   --disable-gpu \
-  --disable-features=AdjustOOMScore \
+  --disable-features=AdjustOOMScore,Translate,BlinkGenPropertyTrees \
   --disable-oom-kill-denial \
   --no-zygote \
   --disable-software-rasterizer \
+  --disable-dev-shm-usage \
   --display=$DISPLAY \
   --no-first-run \
   --start-maximized \
