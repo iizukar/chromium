e67d2a6aa39adbcef4443112bb4c809807078e1e
iizukar <czechia.deitan@gmail.com>
2025-03-14 15:25:30 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# Set display
-export DISPLAY=:1
-
-# Start Xvfb
-Xvfb :1 -screen 0 1280x720x16 &
-
-# Set VNC password (change "password" to your secure password)
+# Set VNC settings
 mkdir -p ~/.vnc
 echo -e "password\npassword\nn" | vncpasswd
 chmod 600 ~/.vnc/passwd
 
-# Start VNC server
+# Start Xvfb and VNC server
+Xvfb :1 -screen 0 1280x720x16 &
+export DISPLAY=:1
+
+# Start window manager and VNC
+fluxbox &
 vncserver :1 -geometry 1280x720 -depth 16 -localhost no \
-  -xstartup /usr/bin/xterm &
+  -xstartup /usr/bin/startfluxbox &
 
 # Start noVNC proxy
 /opt/noVNC/utils/novnc_proxy --vnc localhost:5901 --listen 0.0.0.0:8080 &
 
-# Start Chromium
+# Start Chromium with proper flags
 chromium-browser --no-sandbox --disable-gpu \
   --display=$DISPLAY \
-  --no-first-run --start-maximized \
+  --no-first-run \
+  --start-maximized \
   --window-size=1280,720
